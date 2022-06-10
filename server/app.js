32657fd8ef9e27cb2c2b768ca44ecc22ff2757be
Ryanhustler420 <gouravgupta840@gmail.com>
2022-06-10 13:19:10 -0700

    sadasd

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const winston = require('winston');
 const { app } = require('./connections/express');
 const { SERVER_PORT } = require('./models/_Constants');
-// const socket = require('../server/connections/sockets');
+const socket = require('../server/connections/sockets');
 
-require('../uploads_directory')();
+// require('../uploads_directory')();
 
-require('./startup/environment')();
-require('./startup/logging')();
-require('./startup/config')();
-require('./startup/mkdir')();
+// require('./startup/environment')();
+// require('./startup/logging')();
+// require('./startup/config')();
+// require('./startup/mkdir')();
 // require('./startup/routes')(app);
-require('./connections/database');
+// require('./connections/database');
 
 app.post(`/post`, (req, res) => {
   res.send({
@@ -27,6 +27,6 @@ const PORT = process.env.PORT || SERVER_PORT;
 const server = app.listen(PORT, () => {
     winston.info(`Server is up and running at port: ${PORT}`);
 });
-// socket({ server, app });
+socket({ server, app });
 
 module.exports = app;
