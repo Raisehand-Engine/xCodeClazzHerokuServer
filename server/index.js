9331f92a3b9cf9252fb202ce6811fceaaca051f8
Ryanhustler420 <gouravgupta840@gmail.com>
2022-07-06 15:30:28 -0700

    fixed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const winston = require('winston');
 const { app } = require('./connections/express');
 
-require('./startup/environment')();
-require('./startup/logging')();
-require('./startup/config')();
-require('./startup/mkdir')();
-require('./startup/routes')(app);
+// require('./startup/environment')();
+// require('./startup/logging')();
+// require('./startup/config')();
+// require('./startup/mkdir')();
+// require('./startup/routes')(app);
 require('./connections/database');
 
 app.get('/app', (req, res) => {
